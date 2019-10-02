b371fbbaa3373cc304413709f457586dcd440758
Prajwalsantosh <54844739+Prajwalsantosh@users.noreply.github.com>
2019-10-02 15:39:34 +0530

    Create Week 2 commands of docker_script.sh

---
+++ Week 2 commands of docker_script.sh
@@ -0,0 +1,29 @@
+root@0bd482a7e4ca:/# cd usr/share/nginx/html/
+root@0bd482a7e4ca:/usr/share/nginx/html# cat index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>
+root@0bd482a7e4ca:/usr/share/nginx/html# exit
+exit
